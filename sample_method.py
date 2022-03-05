c7bf6f89d5f8c5389d884b53ec9874a40e7327d8
xiaoshijian <35342641+xiaoshijian@users.noreply.github.com>
2022-03-05 23:44:11 +0800

    Update sample_method.py
    extract transaction for producing feature and train model based on date code

--- sample_method.py
+++ sample_method.py
@@ -56,6 +56,42 @@ def get_dcode_and_ldcode(dataframe, d2c_mapping):
     return dataframe
 
 
+def extract_tran4feat(
+        dataframe,
+        ld4feat,
+        d2c_mapping,
+        tran4feat_sessions=28,
+        is_tran4model=False,
+):
+    '''
+    :param dataframe: transactions
+    :param ld4feat: last date for transaction to generating features
+    :param d2c_mapping: date2code mapping
+    :param tran4feat_sessions: how long session for transaction to generate features
+    :param is_tran4model: if output transaction for building model, if True, the session lasting for
+                          7 days after transactions
+    :return:
+    '''
+
+    ld4feat_code = d2c_mapping[ld4feat]
+    tran4feat_cond = (
+        ((dataframe['dcode'] - ld4feat_code) >= 0) & \
+        ((dataframe['dcode'] - ld4feat_code) < tran4feat_sessions)
+    )
+    tran4feat = dataframe[tran4feat_cond].reset_index(drop=True)
+
+    tran4model = None
+    if is_tran4model == True:
+        tran4model_cond = (
+            (ld4feat_code - dataframe['dcode'] >= 0) & \
+            (ld4feat_code - dataframe['dcode'] < 7)
+
+        )
+        tran4model = dataframe[tran4model_cond].reset_index(drop=True)
+    return (tran4feat, tran4model)
+
+
+
 
 
 
