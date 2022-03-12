5dcef710b144666c373500f43c4cd7d6bed0e624
xiaoshijian <35342641+xiaoshijian@users.noreply.github.com>
2022-03-12 22:24:17 +0800

    Update recommendation.py

--- recall_phrase/item_based_cf/recommendation.py
+++ recall_phrase/item_based_cf/recommendation.py
@@ -1,27 +1,48 @@
-def ibcf_recall(purchased_items, W, K=50, N=50):
-    # 这里的pi是对应不同行为的值的，而在这个数据集中，只有购买这个行为，就直接设置为1
-    pi = 1
-    rank = {}
-    for item in purchased_items:
-        for neighbor, w_neigh in sorted(
-                W[item].items(), key= lambda x: x[1], reverse=True)[0:K]:
-
-            if neighbor in purchased_items:
-                continue
-
-            rank[neighbor] += pi * w_neigh
 
 
-    # sort rank with its similarity
-    rank = sorted(rank.items(), key= lambda x: x[1], reverse=True)[0:N]
-
-    # get rank
-    rank = [item[0] for item in rank]
-    return rank
 
 
-def run_recall(samples, W, K=50, N=50):
-    # user_id_list = train['customer_id'].tolist()
-    samples['ibcf_recall'] = samples['transations_list'].apply(
-        lambda x: ibcf_recall(x), args=(W, K, N,), axis=1)
-    return samples
+def weight_item_by_dcode(dcode, min_dcode):
+    # reduce weight while date is remote
+    return 1 / ((dcode - min_dcode) ** 0.99)
+
+def weight_item_by_ldcode(ldcode, min_ldcode):
+    # session_split的命名可以优化
+    # calculate items weight in recall phrase, set weight to 1 while items purchased in this week
+    # set 1/2 while in last week and 1/3 while in last last week
+    diff = ldcode - min_ldcode
+    return 1 / (1 + diff) if diff <= 2 else 0
+
+def ibcf_recall_method1(purchased_items, W, min_decode, N=50):
+    # 使用三个星期的购买记录作为召回的items，其中tw的weight是1, lw的weight是1/2， llw的weight是1/3
+    # 使用多种不同的weight进行召回
+    # 可能在2个小时左右
+    # set weight    
+    recall_items = {}
+    for item, dcode in purchased_items:
+        if item not in W:
+            continue
+        date_weight = weight_item_by_dcode(dcode, min_decode)
+        for nbor, nbor_weight in W[item]:
+            if nbor not in recall_items:
+                recall_items[nbor] += date_weight * nbor_weight
+    recall_items = sorted(recall_items.items(), key = lambda x: x[1], reverse=True)
+    return recall_items[:N]
+
+def ibcf_recall_method2(purchased_items, W, min_ldcode, N=50):
+    # 使用三个星期的购买记录作为召回的items，其中tw的weight是1, lw的weight是1/2， llw的weight是1/3
+    # 使用多种不同的weight进行召回
+    # 可能在2个小时左右
+    # set weight    
+    recall_items = {}
+    for item, ldcode in purchased_items:
+        if item not in W:
+            continue
+        date_weight = weight_item_by_ldcode(ldcode, min_ldcode)
+        for nbor, nbor_weight in W[item]:
+            if nbor not in recall_items:
+                recall_items[nbor] += date_weight * nbor_weight
+    recall_items = sorted(recall_items.items(), key = lambda x: x[1], reverse=True)
+    return recall_items[:N]
+
+
