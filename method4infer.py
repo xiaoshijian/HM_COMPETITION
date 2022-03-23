024bd9e53f1470e8dc8baf7e03c312f44e55ec70
xiaoshijian <35342641+xiaoshijian@users.noreply.github.com>
2022-03-24 04:39:02 +0800

    Create method4infer.py

---
+++ method4infer.py
@@ -0,0 +1,89 @@
+import pandas as pd
+from .toolbox.decorator import timer
+from .feature_engineer.method4fe import combine_info_and_get_sampples
+
+
+
+# # batch_size:
+# # batch_size --> customer_id, article_id, features_used
+# # predict --> customer_id, article_id, scores, -->  customer_id, (article_id, scores) list
+# # batch of (customer_id, (article_id, scores) list)
+# # output batch
+# # merge in the end
+# # fix according to article
+@timer
+def batch_pred(batch_samples,
+               clf,
+               article_feature_dict,
+               customer_feature_dict,
+               interaction_feature_dict,
+               features_name,
+               max_recommend_num=12):
+    # generate data for prediction
+    data4pred = []
+    for cid, aids in zip(
+            batch_samples['customer_id'], batch_samples['recall_items']
+    ):
+        for aid in aids:
+            info = {}
+            info['customer_id'] = cid
+            info['article_id'] = aid
+            features = combine_info_and_get_sampples(
+                aid,
+                cid,
+                article_feature_dict,
+                customer_feature_dict,
+                interaction_feature_dict,
+            )
+            info.update(features)
+            data4pred.append(info)
+    data4pred = pd.DataFrame(data4pred)
+    data4pred['score'] = clf.predict_proba(data4pred[features_name])[:, 1]
+
+    data4pred['aid_n_score'] = data4pred.apply(
+        lambda x: (x.article_id, x.score), axis=1)
+    # merge and combine records from same customer into list
+    data4pred = data4pred.groupby('customer_id')['aid_n_score'].apply(list).reset_index(name='recommend')
+
+    # sort items in recommen by score
+    data4pred['recommend'] = data4pred['recommend'].apply(
+        lambda x: sorted(x, key=lambda x: x[1], reverse=True)
+    )
+    # extract recommend article
+    data4pred['recommend'] = data4pred['recommend'].apply(
+        lambda x: [item[0] for item in x][:max_recommend_num]
+    )
+    return data4pred
+
+
+def pred(samples,
+         clf,
+         article_feature_dict,
+         customer_feature_dict,
+         interaction_feature_dict,
+         features_name,
+         batch_size=1000,
+         max_recommend_num=12):
+    # tss is abbrevation for total sample size
+    tss = len(samples)
+    prediction = []
+    for i in range(0, tss, batch_size):
+        if i == tss - 1:
+            continue
+        bsp = i
+        bep = min(bsp + batch_size, tss)
+        batch_samples = samples[bsp:bep]
+        prediction.append(
+            batch_pred(
+                batch_samples,
+                clf,
+                article_feature_dict,
+                customer_feature_dict,
+                interaction_feature_dict,
+                features_name,
+                max_recommend_num
+            )
+        )
+        # 是否要加del啊？
+    predicton = pd.concat(prediction, axis=0, ignore_index=True)
+    return predicton
