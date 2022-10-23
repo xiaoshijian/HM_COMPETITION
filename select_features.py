4b5fa08e14934fad3adcbc0b3457ac4e08ca9e15
xiaoshijian <35342641+xiaoshijian@users.noreply.github.com>
2022-10-23 16:58:28 +0800

    Update select_features.py
    add ContinousFeatureFilter

--- select_features.py
+++ select_features.py
@@ -4,6 +4,8 @@ import numpy as np
 import pandas as pd
 import xgboost as xgb
 import lightgbm as lgb
+from sklearn.feature_selection import chi2
+from sklearn.preprocessing import minmax_scale
 
 def get_mv_for_features(data, label):
     model = mvtest()
@@ -110,3 +112,29 @@ def extract_features_from_three_importance_type(
                 occurrences_features.extend(select_features)
             result['occurrences_features'] = list(set(occurrences_features))
         return result 
+    
+    
+ class ContinousFeatureFilter:
+    def __init__(self, colnames2filter, colname4id, colname4label, ratio=0.1):
+        self._colname4id = colname4id
+        self._colname4label = colname4label
+        self._colnames2filter = [col for col in colnames2filter if col not in [self._colname4id, self._colname4label]] 
+        self._ratio = ratio
+        
+    def fit(self, data):
+        # 使用chi2来筛选生成的一系列连续型特征
+        m = len(self._colnames2filter)
+        chi2_values = chi2(
+            # 这种replace的处理方法是不完善的，可以继续改进的
+            minmax_scale(data[self._colnames2filter].replace([np.inf, -np.inf, np.nan], 0)),  # 填入的需要是non-negative的，所以进行minmax
+            data[self._colname4label])[0]  
+        pairs = [[col, value] for col, value in zip(self._colnames2filter, chi2_values.tolist())]
+#         print(pairs)
+        pairs = sorted(pairs, key = lambda x: x[1], reverse=True)
+        # record some infomation why to do feature selection decison, 
+        # features in self._features_chi2info should be more than features in self._selected_features
+        self._features_chi2info = {x[0]: x[1] for x in pairs}  
+        self._selected_features = [x[0] for x in pairs[:int(m * self._ratio)]]
+    
+    def transform(self, data):
+        return data[[self._colname4id] + self._selected_features]
